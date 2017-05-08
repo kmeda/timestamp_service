89737c38865622b178e88267e44b5c9eb5788e24
kmeda <kathikreddymeda@gmail.com>
2017-05-08 10:58:06 +0530

    initial

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var moment = require('moment');
+
+
+var app = express();
+
+app.set('view-engine', 'html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+  res.render('index');
+});
+
+app.get('/:time', (req, res)=>{
+  var time = req.params.time;
+
+  if (/^\d{8,}$/.test(time)) {
+    var timeFormat = moment(time, "X");
+  } else {
+    var timeFormat = moment(time, "MMMM D, YYYY")
+  }
+
+  if (timeFormat.isValid()) {
+    res.send({unix: timeFormat.format('X'), natural: timeFormat.format("MMMM D, YYYY")});
+  } else {
+    res.send({unix: null, natural: null});
+  }
+});
+
+app.listen(3000, ()=>{
+  console.log("Listening on port", 3000);
+});
